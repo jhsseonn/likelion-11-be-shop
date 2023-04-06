444c20cbe0af8b543a24be66002aa90478094816
daeun han <daeunhan0723@gmail.com>
2023-04-06 23:09:20 +0900

    1차 파일 충돌 해결

---
+++ src/main/java/com/likelion/beshop/entity/Member.java
@@ -0,0 +1,44 @@
+package com.likelion.beshop.entity;
+
+import com.likelion.beshop.constant.Role;
+import com.likelion.beshop.dto.MemberFormDto;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="member")
+@Getter @Setter
+@ToString
+
+public class Member {
+    @Id
+    @Column(name="member_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    //기본키가 있는 멤버가 됨
+
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private String address;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public static Member creatMember(MemberFormDto memberFormDto, PasswordEncoder passwordEncoder) {
+        Member member = new Member();
+        member.setName(memberFormDto.getName());
+        member.setEmail(memberFormDto.getEmail());
+        String pwd = passwordEncoder.encode(memberFormDto.getPassword());
+        member.setPassword(pwd);
+        member.setAddress(memberFormDto.getAddress());
+        member.setRole(Role.USER);
+
+        return member;
+    }
+}
