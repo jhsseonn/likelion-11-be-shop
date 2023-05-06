a2e005f70770227a1f784130520bb21f2ffa85ef
fjqmqjrm <fjqmqjrm@naver.com>
2023-05-07 02:59:21 +0900

    seoyoon0508

---
+++ src/main/java/com/likelion/beshop/entity/OrderItem.java
@@ -0,0 +1,35 @@
+package com.likelion.beshop.entity;
+
+import com.likelion.beshop.constant.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="orderitems")
+@Getter
+@Setter
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "orderitem_id")
+    private  Long id;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "item_id")
+    private Item item ; // 주문 상품
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "order_id")
+    private Order order; // 주문 코드
+
+
+    private int orderPrice;//주문 가격
+    private int num; // 주문수량
+
+    private LocalDateTime registrationtime; // 등록시간
+
+    private LocalDateTime correctiontime;// 수정시간
+}
+
