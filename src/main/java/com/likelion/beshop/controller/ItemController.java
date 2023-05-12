eaff008db3dca972e368849c40f3d14a4c5420a8
daeun han <daeunhan0723@gmail.com>
2023-05-12 13:37:36 +0900

    상품 조회까지 작성

--- src/main/java/com/likelion/beshop/controller/ItemController.java
+++ src/main/java/com/likelion/beshop/controller/ItemController.java
@@ -1,36 +1,31 @@
 package com.likelion.beshop.controller;
 
 import com.likelion.beshop.dto.ItemFormDto;
-import com.likelion.beshop.dto.MemberFormDto;
-import com.likelion.beshop.entity.Member;
 import com.likelion.beshop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 import java.util.List;
 
-@RequestMapping("/admin")
 @Controller
 @RequiredArgsConstructor
 public class ItemController {
 
     private final ItemService itemService;
 
-    @GetMapping(value = "/item/new")
+    @GetMapping(value = "/admin/item/new")
     public String itemForm(Model model) {
         model.addAttribute("itemFormDto", new ItemFormDto());
         return "item/itemForm";
     }
 
-    @PostMapping(value = "/item/new")
+    @PostMapping(value = "/admin/item/new")
     public String itemNew(@Valid ItemFormDto itemFormDto, BindingResult bindingResult,
                           Model model, @RequestParam("itemImgFile") List<MultipartFile> itemImgFileList){ // 이미지들의 리스트를 받아오기 위한 파라미터 추가
 
@@ -51,5 +46,13 @@ public class ItemController {
         }
 
         return "redirect:/"; // 정상 등록 시, 메인 페이지로 이동 = /(root)로 리다이렉트
+    }
+
+    @GetMapping(value = "/item/{itemId}")
+    public String itemDtl(@PathVariable("itemId") Long itemId, Model model) { // 상품의 ID에 따라 경로가 달라져야하므로 그에 맞는 어노테이션과 함께 매개변수 설정
+
+        ItemFormDto itemFormDto = itemService.getItemDtl(itemId); // 상품의 ID로 상품 데이터 불러오기
+        model.addAttribute("item", itemFormDto); // 모델에 불러온 상품 데이터를 item이라는 이름으로 담아 데이터 넘겨주기
+        return "item/itemDtl";
     }
 }
--- src/main/java/com/likelion/beshop/service/ItemService.java
+++ src/main/java/com/likelion/beshop/service/ItemService.java
@@ -1,6 +1,8 @@
 package com.likelion.beshop.service;
 
 import com.likelion.beshop.dto.ItemFormDto;
+
+import com.likelion.beshop.dto.ItemImgDto;
 import com.likelion.beshop.entity.Item;
 import com.likelion.beshop.entity.ItemImg;
 import com.likelion.beshop.repository.ItemImgRepository;
@@ -9,7 +11,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.transaction.Transactional;
+import javax.persistence.EntityNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -42,5 +46,23 @@ public class ItemService {
         }
 
         return item.getCode(); // 상품의 Code(ID) 값 리턴
+    }
+
+    @Transactional(readOnly = true) // 트랜잭션 읽기 전용 설정 어노테이션 -> 해당 어노테이션 사용 시, JPA가 더티체킹이라고 하는 변경을 감지하는 행위를 수행하지 않으므로 성능을 향상시킬 수 있어서 이런 함수는 읽기 전용으로 설정
+    public ItemFormDto getItemDtl(Long itemId){ // 상품 데이터를 받아올 함수
+        List<ItemImg> itemImgList = itemImgRepository.findByItemIdOrderByIdAsc(itemId); // 상품에 해당하는 이미지 조회
+        List<ItemImgDto> itemImgDtoList = new ArrayList<>(); // 매개변수로 받은 인스턴스를 이용해 새로운 인스턴스 생성 (ModelMapper)
+        for (ItemImg itemImg : itemImgList) {
+            ItemImgDto itemImgDto = ItemImgDto.of(itemImg);
+            itemImgDtoList.add(itemImgDto); // 받아온 이미지들을 ItemImgDto 객체로 만들어서 List에 추가
+        }
+
+        //) 상품의 ID로 상품 엔티티 조회
+        Item item = itemRepository.findById(itemId)
+                .orElseThrow(EntityNotFoundException::new);
+        ItemFormDto itemFormDto = ItemFormDto.of(item); // 받아온 상품은 ItemFormDto 객체로 만들기
+        //  ItemFormDto 객체에 이미지 리스트 세팅 후 반환
+        itemFormDto.setItemImgDtoList(itemImgDtoList);
+        return itemFormDto;
     }
 }
