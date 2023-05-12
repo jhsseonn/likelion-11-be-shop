50c2476d7578bd2f284e308d6bf3cf347eab0531
daeun han <daeunhan0723@gmail.com>
2023-05-12 13:44:40 +0900

    1차 해결

--- src/main/java/com/likelion/beshop/controller/ItemController.java
+++ src/main/java/com/likelion/beshop/controller/ItemController.java
@@ -9,7 +9,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 import java.util.List;
 
