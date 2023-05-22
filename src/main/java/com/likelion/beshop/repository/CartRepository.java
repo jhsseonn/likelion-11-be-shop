c02dd1afda6da0246c4264d46cf7786b6d722841
daeun han <daeunhan0723@gmail.com>
2023-05-22 21:27:09 +0900

    0522 정기세션 실습

--- src/main/java/com/likelion/beshop/repository/CartRepository.java
+++ src/main/java/com/likelion/beshop/repository/CartRepository.java
@@ -1,14 +1,11 @@
 package com.likelion.beshop.repository;
 
 import com.likelion.beshop.entity.Cart;
-import com.likelion.beshop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
+    Cart findByMemberId(Long memberId); // 멤버 아이디로 장바구니 조회
 
 }
